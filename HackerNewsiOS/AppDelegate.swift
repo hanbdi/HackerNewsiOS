eff1a41fbc281e2a8c2346c7eb52751fb7e0b1b2
Henry Hung Dang <dangduonghung@gmail.com>
2020-02-16 10:45:51 +0800

    Init project, target 10.0

---
+++ HackerNewsiOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HackerNewsiOS
+//
+//  Created by Dang Duong Hung on 16/2/20.
+//  Copyright © 2020 Dang Duong Hung. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
